da0c14274a0603b73a2f235a2d12717202b4910e
Srikar Akkina <srikarakkina@Srikars-MacBook-Air.local>
2025-12-26 17:44:31 +0530

    music

--- src/components/youtube/YouTubeCard.jsx
+++ src/components/youtube/YouTubeCard.jsx
@@ -47,6 +47,15 @@ const YouTubeCard = ({ data, trackId, handlePlay }) => {
                         {data.name || "Unknown Title"}
                     </div>
 
+                    {data.lyrics && (
+                        <div className="lyrics-symbol" title="Lyrics Available">
+                            <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
+                                <path d="M12 14c1.66 0 3-1.34 3-3V5c0-1.66-1.34-3-3-3S9 2.34 9 5v6c0 1.66 1.34 3 3 3z" />
+                                <path d="M17 11c0 2.76-2.24 5-5 5s-5-2.24-5-5H5c0 3.53 2.61 6.43 6 6.92V21h2v-3.08c3.39-.49 6-3.39 6-6.92h-2z" />
+                            </svg>
+                        </div>
+                    )}
+
                     {/* Green Spotify Play Button */}
                     {trackId && (
                         <div
