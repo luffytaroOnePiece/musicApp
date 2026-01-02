f51fefec60a0e7f23fc8b768c163a8cf78e7b0dc
Srikar Akkina <srikarakkina@Srikars-MacBook-Air.local>
2026-01-02 19:53:23 +0530

    music

--- src/components/albums/AlbumDetail.jsx
+++ src/components/albums/AlbumDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, useMemo } from 'react';
 import YouTubeCard from '../youtube/YouTubeCard';
 import AlbumCard from './AlbumCard';
 import '../../styles/albums/AlbumDetail.css';
